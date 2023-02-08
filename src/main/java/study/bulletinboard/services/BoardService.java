73e6a00d150569456c1bf0730884ee88391605c0
YHCHOI <cyh6809@gmail.com>
2023-02-08 23:47:04 +0900

    Controller, Service, Repository, DTO 생성 HTTP Method 생성

---
+++ src/main/java/study/bulletinboard/services/BoardService.java
@@ -0,0 +1,9 @@
+package study.bulletinboard.services;
+
+import org.springframework.stereotype.Service;
+import study.bulletinboard.repository.BoardRepository;
+
+@Service
+public class BoardService {
+    private BoardRepository repository;
+}
