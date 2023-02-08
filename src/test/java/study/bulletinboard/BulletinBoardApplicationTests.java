4850f1992833ac4ae23650e8b9f970548873b737
YHCHOI <cyh6809@gmail.com>
2023-02-08 20:31:38 +0900

    init

---
+++ src/test/java/study/bulletinboard/BulletinBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package study.bulletinboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulletinBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
