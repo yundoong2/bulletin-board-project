a91dba74f046831b9f2fc91f1aee18cd08e6c701
YHCHOI <cyh6809@gmail.com>
2023-02-10 23:54:54 +0900

    게시글 등록, 전체 게시글 조회, 특정 게시글 조회 메소드 구현

--- src/main/java/study/bulletinboard/repository/BoardRepository.java
+++ src/main/java/study/bulletinboard/repository/BoardRepository.java
@@ -1,8 +1,12 @@
 package study.bulletinboard.repository;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import study.bulletinboard.entity.BoardEntity;
 
-@Repository
-public class BoardRepository {
+public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
 
+    /*  JpaRepository 에서 기본적으로 제공하는 메소드 외에 필요한 부분은 아래처럼 정의해서 사용 가능
+    public BoardEntity findByTitle(String title);
+    public BoardEntity findByTitleAndWriter(String title, String writer);
+    */
 }
