949c11c4bb78f255ad19cbe8a548e9ddd175ef37
YHCHOI <cyh6809@gmail.com>
2023-02-12 01:03:21 +0900

    게시물 전체 수정, 게시물 부분 수정 구현, H2 DB 연동 및 각 메소드 테스트

--- src/main/java/study/bulletinboard/entity/BoardEntity.java
+++ src/main/java/study/bulletinboard/entity/BoardEntity.java
@@ -1,7 +1,12 @@
 package study.bulletinboard.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicInsert;
 import org.springframework.util.Assert;
 
 import javax.persistence.*;
@@ -12,6 +17,8 @@ import java.time.LocalDateTime;
 @Entity
 @Builder
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "BOARD") //MySQL 8.x 기준으로 해당 버전 이상부터는 테이블 명을 무조건 대문자로 해야함
 public class BoardEntity {
     @Id
@@ -24,9 +31,17 @@ public class BoardEntity {
     private String content;
     @Column(length = 20, nullable = false, name = "WRITER")
     private String writer;
-    @Column(name = "CREATED_AT")
+    @Column(name = "CREATED_AT", nullable = false)
+    @CreationTimestamp
     private LocalDateTime regDate = LocalDateTime.now();
     @Column(name = "HIT")
     private Long hit;
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
